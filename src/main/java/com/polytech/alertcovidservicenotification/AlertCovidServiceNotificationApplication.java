76f21e293f4d16dfbf3091802e8e834af1e71921
Remi Salmi <remi.salmi@gmail.com>
2020-10-24 17:32:05 +0200

    init project

---
+++ src/main/java/com/polytech/alertcovidservicenotification/AlertCovidServiceNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidservicenotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertCovidServiceNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertCovidServiceNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/polytech/alertcovidservicenotification/AlertCovidServiceNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polytech.alertcovidservicenotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertCovidServiceNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

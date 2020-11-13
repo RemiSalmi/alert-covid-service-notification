742f11fc6fedb51dac5d2c4d7f017980cbaa6f63
Florian Smague <smague.florian@lundimatin.fr>
2020-11-13 14:40:43 +0100

    Version 1.1

--- src/main/java/com/polytech/alertcovidservicenotification/models/ContactLocationRepository.java
+++ src/main/java/com/polytech/alertcovidservicenotification/models/ContactLocationRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface ContactLocationRepository extends JpaRepository<ContactLocation,ContactLocationId> {
-    @Query(value = "SELECT * FROM contact_location_history Where id_user = ?1 ORDER BY date Limit 1",nativeQuery = true)
+    @Query(value = "SELECT * FROM contact_location_history Where id_user = ?1 ORDER BY date DESC Limit 1",nativeQuery = true)
     ContactLocation getLastLocationHistory(long id_user);
 }
